7a4bc78b71b8bc1c43868a984062ffa63496266f
DIVYA ROSE RAJU <divyaroseraju@gmail.com>
2018-10-01 10:56:06 +0530

    Update lex.c

--- Day 1/lex.c
+++ Day 1/lex.c
@@ -5,7 +5,11 @@
 
 int iskey(char buffer[])
 {
-	char key[2][10]={"int","char"};
+	char key[2][10]={"auto","break","case","char","const","continue","default",
+                            "do","double","else","enum","extern","float","for","goto",
+                            "if","int","long","register","return","short","signed",
+                            "sizeof","static","struct","switch","typedef","union",
+                            "unsigned","void","volatile","while"};
 	int i,flag=0;
 	for(i=0; i<2;i++){
         if(strcmp(key[i], buffer) == 0){

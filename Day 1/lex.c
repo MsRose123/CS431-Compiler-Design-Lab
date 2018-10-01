767d8a7a440622fe6dc5164ea4e02c70d0e811fe
DIVYA ROSE RAJU <divyaroseraju@gmail.com>
2018-10-01 10:52:17 +0530

    Update lex.c

--- Day 1/lex.c
+++ Day 1/lex.c
@@ -1,152 +1,137 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdio.h>
 #include<ctype.h>
-void main()
-{
-	int state=0,flag=0,i,p=0,id=0;
-	char ch,word[20],kw[20][20]={"int","float","char","long","double","if","else","for","while","void","do","switch","case","break"}; 
-	
-	FILE *f;
-	f=fopen("input.txt","r");
-	while(flag!=1)
-	{	
-		switch(state)
-		{
-			case 0:
-				ch=fgetc(f);
-				if(isalnum(ch))
-				{
-					if(isalpha(ch))
-						state=11;
-					else
-						state=13;
-				}
-				else if(ch=='<')
-				{
-					state=1; 
-				}
-				else if(ch=='>')
-				{
-					state=4;
-				}
-				else if(ch=='!')
-				{
-					state=7; 
-				}
-				else if(ch=='=')
-				{
-					state=9; 
-				}
-				
-				break;
-			case 1:
-				ch=fgetc(f);
-				if(ch=='=')
-					state=2;
-				else
-					state=3;
-				break;
-			case 2:	
-				printf("\n'<=' is a relational operator.");
-				state=0;
-				break;
-			case 3:
-				fseek(f,-1,SEEK_CUR);
-				printf("\n'<' is a relational operator.");
-				state=0;
-				break;
-			case 4:
-				ch=fgetc(f);
-				if(ch=='=')
-					state=5;
-				else
-					state=6;
-				break;
-			case 5:
-				printf("\n'>=' is a relational operator.");
-				state=0;
-				break;
-			case 6:
-				fseek(f,-1,SEEK_CUR);
-				printf("\n'>' is a relational operator.");
-				state=0;
-				break;
-			case 7:
-				ch=fgetc(f);
-				if(ch=='=')
-					state=8;
-				else
-				{
-					fseek(f,-1,SEEK_CUR);
-					state=0;
-				}
-				break;
-			case 8:
-				printf("\n'!=' is a relational operator.");
-				state=0;
-				break;	
-			case 9:
-				ch=fgetc(f);
-				if(ch=='=')
-					state=8;
-				else
-				{
-					fseek(f,-1,SEEK_CUR);
-					state=0;
-				}
-				break;
-			case 10:
-				printf("\n'==' is a relational operator.");
-				state=0;
-				break;	
-			case 11:
-				word[p++]=ch;
-				while(isalnum(ch=fgetc(f)))
-				{
-					word[p++]=ch;
-				}
-				fseek(f,-1,SEEK_CUR);
-				word[p]='\0';
-				state=12;
-				p=0;
-				break;
-			case 12:
-				for(i=0;i<14;i++)
-					if(strcmp(kw[i],word)==0)
-					{
-						printf("\n%s is a keyword.",word);
-						id=1;
-						break;
-					}
-				if(id==0)
-					printf("\n%s is an identifier.",word);
-				state=0;
-				id=0;
-				break;
 
-			case 13:
-				word[p++]=ch;
-				while(isdigit(ch=fgetc(f)))
-				{
-					word[p++]=ch;
-				}
-				fseek(f,-1,SEEK_CUR);
-				word[p]='\0';
-				state=14;
-				p=0;
-				break;
-			case 14:
-				printf("\n%s is a number.",word);
-				state=0;
-				break;
-			default:
-				break;
-		}
-		if(ch==EOF)
-			flag=1;
+int iskey(char buffer[])
+{
+	char key[2][10]={"int","char"};
+	int i,flag=0;
+	for(i=0; i<2;i++){
+        if(strcmp(key[i], buffer) == 0){
+            flag = 1;
+            break;
+        }
 	}
-	printf("\n");
-	fclose(f);
-}				
+	return flag;
+}
+void main()
+{	
+	char ch,buffer[10];
+	int i,j,state;
+	FILE *f;
+	f=fopen("trial.txt","r");
+	if(f==NULL)
+		printf("\nError opening the file");
+	
+while(ch!= EOF)
+{
+	switch(state)
+	{	
+		case 0:ch=fgetc(f);
+			if(ch=='<')state=1;
+			else if(ch=='>')state=4;
+			else if(ch=='!')state=7;		
+			else if(ch=='=')state=9;
+			else if(isalpha(ch))
+			{	buffer[0]=ch;
+				state=11;
+			}
+			else if(isdigit(ch))
+			{	buffer[0]=ch;
+				state=13;
+			}
+			break;
+
+		case 1:ch=fgetc(f);
+			if(ch=='=')state=2;
+			else
+			{	state=3;
+				fseek(f,-1,SEEK_CUR);
+			}
+			break;
+		
+		case 2:state=0;
+			printf("\n<= Relational operator:Lessthan or Equal to");
+			break;
+		
+		case 3:state=0;
+			printf("\n< Relational operator:Lessthan");
+			break;
+
+		case 4:ch=fgetc(f);
+			if(ch=='=')state=5;
+			else
+			{	state=6;
+				fseek(f,-1,SEEK_CUR);
+			}
+			break;
+
+		case 5:state=0;
+			printf("\n>= Relational operator:Greaterthan or Equal to");
+			break;
+		
+		case 6:state=0;
+			printf("\n> Relational operator:Greaterthan");
+			break;
+
+		case 7:ch=fgetc(f);
+			if(ch=='=')state=8;
+			else
+			{	state=0;
+				fseek(f,-1,SEEK_CUR);
+			}
+			break;
+
+		case 8:state=0;
+			printf("\n!= Relational operator:Not Equal to");
+			break;
+
+		case 9:ch=fgetc(f);
+			if(ch=='=')state=10;
+			else
+			{	state=0;
+				fseek(f,-1L,SEEK_CUR);
+			}
+			break;
+
+		case 10:state=0;
+			printf("\n== Relational operator:Equal to");
+			break;
+
+		case 11:j=1;
+			while(isalnum(ch=fgetc(f)))
+			{	buffer[j++]=ch;
+			}
+			fseek(f,-1,SEEK_CUR);
+			state=12;
+			break;
+
+		case 12:state=0;
+			if(iskey(buffer)==0)
+				printf("\n%s is an Identifier",buffer);
+			else
+				printf("\n%s is an keyword",buffer);
+			memset(buffer,0,10);
+			break;
+			
+		case 13:j=1;
+			while(isdigit(ch=fgetc(f)))
+			{	buffer[j++]=ch;
+			}
+			fseek(f,-1,SEEK_CUR);
+			state=14;
+			break;
+
+		case 14:state=0;
+			printf("\n%s is a Number",buffer);
+			memset(buffer,0,10);
+			break;
+
+		default:state=0;
+	}
+}
+printf("\n");
+}
+
 					
